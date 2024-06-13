187c11dbbdad935955a23a89d0400db2a0886158
Beniamin Cosma <cosma.beny@gmail.com>
2024-06-14 00:23:53 +0300

    Whitelist xPortalTeam for push notifications

--- src/mod_push_notification_interceptor.erl
+++ src/mod_push_notification_interceptor.erl
@@ -1,22 +1,27 @@
 -module(mod_push_notification_interceptor).
+
 -behaviour(gen_mod).
 
 %% Required by ?INFO_MSG macros
 -include("logger.hrl").
-
 %% Required by ?T macro
 -include("translate.hrl").
 -include("ejabberd_commands.hrl").
 -include("ejabberd_sm.hrl").
+
 -include_lib("xmpp/include/xmpp.hrl").
+
 -include("mod_mam.hrl").
+
 -include_lib("amqp_client/include/amqp_client.hrl").
+
 -include("mod_roster.hrl").
 
 -type c2s_state() :: ejabberd_c2s:state().
 
 %% gen_mod API callbacks
--export([start/2, stop/1, depends/2, mod_opt_type/1, mod_options/1, mod_doc/0, send_message_to_rb/1]).
+-export([start/2, stop/1, depends/2, mod_opt_type/1, mod_options/1, mod_doc/0,
+         send_message_to_rb/1]).
 
 start(Host, Opts) ->
     ?INFO_MSG("Initialising Push Notification module with options ~p.", [Opts]),
@@ -36,16 +41,13 @@ start(Host, Opts) ->
     %     verify => element(2, lists:nth(4, SslOptions)),
     %     fail_if_no_peer_cert => element(2, lists:nth(5, SslOptions))
     % },
-
-    {ok, Connection} = amqp_connection:start(
-        #amqp_params_network{
-            username = list_to_binary(RabbitUser), 
-            password = list_to_binary(RabbitPassword),
-            host = RabbitHost, 
-            virtual_host = list_to_binary(RabbitVHost),
-            port = RabbitPort,
-            ssl_options = SslOptions
-        }),
+    {ok, Connection} =
+        amqp_connection:start(#amqp_params_network{username = list_to_binary(RabbitUser),
+                                                   password = list_to_binary(RabbitPassword),
+                                                   host = RabbitHost,
+                                                   virtual_host = list_to_binary(RabbitVHost),
+                                                   port = RabbitPort,
+                                                   ssl_options = SslOptions}),
     ?INFO_MSG("RabbitMq connection opened: ~p", [Connection]),
 
     {ok, Channel} = amqp_connection:open_channel(Connection),
@@ -82,24 +84,21 @@ mod_opt_type(rabbit_user) ->
 mod_opt_type(rabbit_password) ->
     econf:string();
 mod_opt_type(ssl_options) ->
-    econf:list(econf:any()).
-
+    econf:list(
+        econf:any()).
 
 mod_options(_Host) ->
-    [
-        {rabbit_port, 5672},
-        {rabbit_host, "host.docker.internal"},
-        {rabbit_vhost, "/"},
-        {rabbit_exchange, "all_events"},
-        {rabbit_exchange_type, "direct"},
-        {rabbit_user, "guest"},
-        {rabbit_password, "guest"},
-        {ssl_options, none}
-    ].
+    [{rabbit_port, 5672},
+     {rabbit_host, "host.docker.internal"},
+     {rabbit_vhost, "/"},
+     {rabbit_exchange, "all_events"},
+     {rabbit_exchange_type, "direct"},
+     {rabbit_user, "guest"},
+     {rabbit_password, "guest"},
+     {ssl_options, none}].
 
 mod_doc() ->
-    #{desc =>
-          ?T("This is an example module.")}.
+    #{desc => ?T("This is an example module.")}.
 
 % get_message_preview(SubEls) ->
 %     get_message_preview(SubEls, []).
@@ -109,66 +108,84 @@ get_message_preview([{xmlel, <<"message-preview">>, _, [{xmlcdata, Data}]} | _])
     Data;
 get_message_preview([_ | Tail]) ->
     get_message_preview(Tail);
-get_message_preview([]) -> 
+get_message_preview([]) ->
     none.
 
--spec send_message_to_rb({stanza(), c2s_state()})
-      -> {stanza(), c2s_state()}.
-send_message_to_rb({#message{from = From, to = To, type = Type, sub_els = SubEls} = Pkt, C2SState}) ->
+-spec send_message_to_rb({stanza(), c2s_state()}) -> {stanza(), c2s_state()}.
+send_message_to_rb({#message{from = From,
+                             to = To,
+                             type = Type,
+                             sub_els = SubEls} =
+                        Pkt,
+                    C2SState}) ->
     ?INFO_MSG("Received message packet ~p", [Pkt]),
 
     % Get the channel connection
     [{_, Channel}] = ets:lookup(my_table, notif_channel),
     [{_, Exchange}] = ets:lookup(my_table, rabbit_exchange),
-    
+
     % Get the message body, set it to empty string if none
     Body = Pkt#message.body,
-    EncryptedMessage = try lists:nth(1, Body) of
-        {_, _, EncryptedMessageFun} -> EncryptedMessageFun
-    catch _:_ -> <<"">>
-    end,
-    
+    EncryptedMessage =
+        try lists:nth(1, Body) of
+            {_, _, EncryptedMessageFun} ->
+                EncryptedMessageFun
+        catch
+            _:_ ->
+                <<"">>
+        end,
+
     % Get the roster items
     Items = mod_roster:get_roster(From#jid.luser, From#jid.lserver),
     ToFind = To#jid.luser,
 
     % Get the subscription item and check if subscription is both
-    SubscriptionItem = lists:filter(fun(Roster) -> element(1, Roster#roster.jid) == ToFind end, Items),
+    SubscriptionItem =
+        lists:filter(fun(Roster) -> element(1, Roster#roster.jid) == ToFind end, Items),
     % ?INFO_MSG("Subscription item is ~p", [SubscriptionItem]),
-
     % Take subscription value
-    Subscription = case SubscriptionItem of
-        [Item] -> Item#roster.subscription;
-        [] -> false;
-        _ -> false
-    end,
+    Subscription =
+        case SubscriptionItem of
+            [Item] ->
+                Item#roster.subscription;
+            [] ->
+                false;
+            _ ->
+                false
+        end,
 
     % Get preview message content or empty if none
     MessagePreviewContent = get_message_preview(SubEls),
     % ?INFO_MSG("Preview message is ~p", [MessagePreviewContent]),
-
     % Check if the receiver user is online
-    IsToUserOffline =  case ejabberd_sm:get_user_present_resources(To#jid.luser, To#jid.lserver) of
-        [] -> true;
-        _ -> false
-    end,
+    IsToUserOffline =
+        case ejabberd_sm:get_user_present_resources(To#jid.luser, To#jid.lserver) of
+            [] ->
+                true;
+            _ ->
+                false
+        end,
 
     % ?INFO_MSG("Presence is ~p", [IsToUserOffline]),
     % Check if the message is a chat message and the subscription is both
-    if Type == chat andalso Subscription == both andalso MessagePreviewContent /= none
-        andalso IsToUserOffline == true ->
-        PayloadStruct = #{
-            message => MessagePreviewContent,
-            senderAddress => From#jid.luser,
-            receiverAddress => To#jid.luser
-            },
-        
-        % JSON encode the payload
-        Payload = jiffy:encode(PayloadStruct),
-        Publish = #'basic.publish'{exchange = Exchange, routing_key = <<"">>},
-        amqp_channel:cast(Channel, Publish, #amqp_msg{payload=Payload}),
-        ?INFO_MSG("Message sent to RabbitMQ: ~p", [Payload]);
-    true -> false
+    if Type == chat
+       andalso Subscription == both
+       andalso MessagePreviewContent /= none
+       andalso IsToUserOffline == true
+       orelse From#jid.luser
+              == <<"erd1lpc6wjh2hav6q50p8y6a44r2lhtnseqksygakjfgep6c9uduchkqphzu6t">> ->
+           PayloadStruct =
+               #{message => MessagePreviewContent,
+                 senderAddress => From#jid.luser,
+                 receiverAddress => To#jid.luser},
+
+           % JSON encode the payload
+           Payload = jiffy:encode(PayloadStruct),
+           Publish = #'basic.publish'{exchange = Exchange, routing_key = <<"">>},
+           amqp_channel:cast(Channel, Publish, #amqp_msg{payload = Payload}),
+           ?INFO_MSG("Message sent to RabbitMQ: ~p", [Payload]);
+       true ->
+           false
     end,
 
     {Pkt, C2SState};
